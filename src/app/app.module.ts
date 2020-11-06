037b84c7edc84d6f2ff75b68458d2cdfe2ed9e75
paul charensol <charensolp@gmail.com>
2020-11-06 18:10:22 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
